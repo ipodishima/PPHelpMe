f9eff2f7d09bfdebdef07c37dcb63066a5d32d1c
Jérémy Lagrue <jeremy@ipup.pro>
2013-10-23 16:54:16 -0400

    old but usefull files added

---
+++ PPHelpMe/PPHelpMeFiles/PPGlobalNavigatorMetrics.h
@@ -0,0 +1,21 @@
+//
+//  PPGlobalNavigatorMetrics.h
+//  PhotoScroller
+//
+//  Created by Marian PAUL on 03/11/10.
+//  Copyright 2010 iPuP SARL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+CGRect PPScreenBounds();
+CGRect PPScreenBoundsWithOrientation(UIInterfaceOrientation orientation);
+
+CGRect PPNavigationFrame();
+
+CGFloat PPToolBarHeight();
+
+CGFloat PPStatusBarHeight();
+CGFloat PPTabBarHeight();
